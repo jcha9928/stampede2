01199e33daf07629e8e5fb341a21bfc5b84703fa
Jiook Cha <cha.jiook@gmail.com>
2017-09-10 21:32:34 -0400

    Create batch.sh

---
+++ batch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J test
+#SBATCH -N 4
+#SBATCH -n 4
+#SBATCH -p development
+#SBATCH -o test.o%j
+#SBATCH -t 00:30:00
+#          <------ Account String ----->
+# <--- (Use this ONLY if you have MULTIPLE accounts) --->
+#SBATCH -A A-ccsc
+
+export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
+export LAUNCHER_RMI=SLURM
+export LAUNCHER_JOB_FILE=joblist
+
+$LAUNCHER_DIR/paramrun
