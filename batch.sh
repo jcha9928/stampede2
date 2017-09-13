e5382533ecef29293caff84b59d75ead95e99da3
Jiook Cha <cha.jiook@gmail.com>
2017-09-12 20:25:02 -0400

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -3,13 +3,14 @@
 #SBATCH -N 4
 #SBATCH -n 4
 #SBATCH -p development
-#SBATCH -o ./tcken.o%j
-#SBATCH -e ./tcken.e%j
+#SBATCH -o tcken.o%j                                                                                                  
+#SBATCH -e tcken.e%j 
 #SBATCH -t 48:00:00
-#SBATCH --mail-user=cha.jiook@gmail.com                         
+#SBATCH --mail-user=cha.jiook@gmail.com
 
+module load launcher
 export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
 export LAUNCHER_RMI=SLURM
-export LAUNCHER_JOB_FILE=joblist
+export LAUNCHER_JOB_FILE=/work/03263/jcha9928/stampede2/code/stampede2/joblist
 
 $LAUNCHER_DIR/paramrun
